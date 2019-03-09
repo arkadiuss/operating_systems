5708a76a94027d6b2ca96719f9c02d9ca2f6a8b8
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-09 19:11:18 +0100

    basic makefile

---
+++ 1_file_managing_library/library_loader.c
@@ -0,0 +1,11 @@
+//
+// Created by arkadius on 09.03.19.
+//
+
+#include <dlfcn.h>
+#include "library_loader.h"
+
+void load_library() {
+    //void *handle = dlopen("", RTLD_LAZY);
+}
+
---
+++ 1_file_managing_library/library_loader.h
@@ -0,0 +1,9 @@
+//
+// Created by arkadius on 09.03.19.
+//
+
+#ifndef INC_1_FILE_MANAGING_LIBRARY_LIBRARY_LOADER_H
+#define INC_1_FILE_MANAGING_LIBRARY_LIBRARY_LOADER_H
+
+void load_library();
+#endif //INC_1_FILE_MANAGING_LIBRARY_LIBRARY_LOADER_H
