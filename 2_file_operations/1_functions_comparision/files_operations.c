141ff1be2d7604d2f49f51626a67a8b9816a469f
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-03-16 18:08:59 +0100

    [2] copying files

--- 2_file_operations/1_functions_comparision/files_operations.c
+++ 2_file_operations/1_functions_comparision/files_operations.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
 #include "files_operations.h"
 
 
@@ -18,9 +19,9 @@ void fill_with_random(char *record, int record_size){
 int generate(char *file_name, int records_count, int record_size) {
     time_t t;
     srand(time(&t));
-    int fd = open(file_name, O_CREAT | O_WRONLY);
+    int fd = open(file_name, O_RDWR|O_CREAT, S_IRWXU|S_IRGRP|S_IROTH);
     if(fd == -1) {
-        fprintf(stderr, "Error while opening file");
+        fprintf(stderr, "Error while opening file %d", errno);
         return -1;
     }
     char* record = malloc(sizeof(char)*record_size);
@@ -35,6 +36,7 @@ int generate(char *file_name, int records_count, int record_size) {
 }
 
 int sys_sort(char *file_name, int records_count, int record_size) {
+
     return 0;
 }
 
@@ -43,9 +45,35 @@ int lib_sort(char *file_name, int records_count, int record_size) {
 }
 
 int sys_copy(char *file_name_src, char *file_name_dest, int records_count, int record_size) {
+    int fd_src = open(file_name_src, O_RDONLY);
+    int fd_dest = open(file_name_dest, O_RDWR|O_CREAT, S_IRWXU|S_IRGRP|S_IROTH);
+    if(fd_src == -1 || fd_dest == -1) {
+        fprintf(stderr, "Unable to open src or dest file");
+        return -1;
+    }
+    char* record = malloc(sizeof(char)*record_size);
+    ssize_t readed_size;
+    while(records_count-- &&
+        (readed_size = read(fd_src, record, record_size)) != 0) {
+        write(fd_dest, record, readed_size);
+    }
+    free(record);
     return 0;
 }
 
 int lib_copy(char *file_name_src, char *file_name_dest, int records_count, int record_size) {
+    FILE *src = fopen(file_name_src, "r");
+    FILE *dest = fopen(file_name_dest, "w");
+    if(src == NULL || dest == NULL) {
+        fprintf(stderr, "Unable to open src or dest file");
+        return -1;
+    }
+    char* record = malloc(sizeof(char)*record_size);
+    ssize_t readed_size;
+    while(records_count-- &&
+        (readed_size = fread(record, sizeof(char), record_size, src)) != 0) {
+        fwrite(record, sizeof(char), readed_size, dest);
+    }
+    free(record);
     return 0;
 }
